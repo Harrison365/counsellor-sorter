449d861401f77cff550f56687baeed857eed25b6
Harrison Royds <harrisonroyds@Harrisons-MacBook-Pro.local>
2022-10-07 19:23:37 +0100

    counsellors entered and jest installed

---
+++ counsellors.js
@@ -0,0 +1,86 @@
+const counsellors = [
+  {
+    name: "Emily",
+    p1: "swimming",
+    p2: "singing",
+    cabin: "B1",
+  },
+  {
+    name: "Ben",
+    p1: "tennis",
+    p2: "pottery",
+    cabin: "B1",
+  },
+  {
+    name: "Dan",
+    p1: "running",
+    p2: "singing",
+    cabin: "B2",
+  },
+  {
+    name: "Becca",
+    p1: "bracelet-making",
+    p2: "martial-arts",
+    cabin: "B2",
+  },
+  {
+    name: "Nial",
+    p1: "whaling",
+    p2: "martial-arts",
+    cabin: "B3",
+  },
+  {
+    name: "Sarah",
+    p1: "kanooing",
+    p2: "free",
+    cabin: "B3",
+  },
+  {
+    name: "James",
+    p1: "whaling",
+    p2: "free",
+    cabin: "B4",
+  },
+  {
+    name: "Craig",
+    p1: "swimming",
+    p2: "camping",
+    cabin: "B4",
+  },
+  {
+    name: "Alana",
+    p1: "kanooing",
+    p2: "rugby",
+    cabin: "B5",
+  },
+  {
+    name: "Luke",
+    p1: "free",
+    p2: "camping",
+    cabin: "B5",
+  },
+  {
+    name: "Josh",
+    p1: "free",
+    p2: "zumba",
+    cabin: "B6",
+  },
+  {
+    name: "Mack",
+    p1: "bracelet-making",
+    p2: "rugby",
+    cabin: "B6",
+  },
+  {
+    name: "Jeff",
+    p1: "running",
+    p2: "pottery",
+    cabin: "B7",
+  },
+  {
+    name: "Kim",
+    p1: "tennis",
+    p2: "zumba",
+    cabin: "B7",
+  },
+];
---
+++ periods.js
@@ -0,0 +1,36 @@
+let period1 = [
+  "swimming",
+  "tennis",
+  "running",
+  "bracelet-making",
+  "whaling",
+  "kanooing",
+  "free",
+];
+let period2 = [
+  "rugby",
+  "singing",
+  "pottery",
+  "zumba",
+  "martial-arts",
+  "camping",
+  "free",
+];
+// let period3 = [
+//   "archery",
+//   "bee-keeping",
+//   "running",
+//   "bracelet-making",
+//   "swimming",
+//   "tennis",
+//   "free",
+// ];
+// let period4 = [
+//   "swimming",
+//   "tennis",
+//   "running",
+//   "bracelet-making",
+//   "swimming",
+//   "tennis",
+//   "free",
+// ];
