124297590c993a19fc6093a5f6f780ea6569dbc6
Harrison365 <harrisonroyds@yahoo.co,uk>
2022-10-09 16:04:24 +0100

    randomised

--- sorter.js
+++ sorter.js
@@ -1,36 +1,19 @@
 const { counsellors } = require("./counsellors.js");
 
 const sorter = (counsellors) => {
-  console.log(counsellors.length);
-  let mondayArray = [];
-  let tuesdayArray = [];
-  let wednesdayArray = [];
-  let thursdayArray = [];
+  let shuffledCounsellors = counsellors.sort(() => Math.random() - 0.5);
 
-  for (let i = 0; i < counsellors.length; i++) {
-    let randomNumber = Math.floor(Math.random() * (4 - 1 + 1)) + 1;
+  let mondayArray = ["M"];
+  let tuesdayArray = ["Tu"];
+  let wednesdayArray = ["W"];
+  let thursdayArray = ["Th"];
 
-    if (randomNumber === 1 && mondayArray.length < 4) {
-      mondayArray.push(counsellors[i]);
-    }
-    if (randomNumber === 2 && tuesdayArray.length < 4) {
-      tuesdayArray.push(counsellors[i]);
-    }
-    if (randomNumber === 3 && wednesdayArray.length < 4) {
-      wednesdayArray.push(counsellors[i]);
-    }
-    if (randomNumber === 4 && thursdayArray.length < 4) {
-      thursdayArray.push(counsellors[i]);
-    }
+  let weekArray = [mondayArray, tuesdayArray, wednesdayArray, thursdayArray];
+  let shuffledWeekArray = weekArray.sort(() => Math.random() - 0.5);
 
-    //NEED A WAY TO LOOP BACK UP TO PICK A NEW NUMBER IF ARRAY IS FULL
-  }
-  console.log(
-    mondayArray.length +
-      tuesdayArray.length +
-      wednesdayArray.length +
-      thursdayArray.length
-  );
+  console.log(shuffledWeekArray, shuffledCounsellors);
+  for (let i = 0; i < shuffledCounsellors.length; i++) {}
+
   return {
     Monday: mondayArray,
     Tuesday: tuesdayArray,
