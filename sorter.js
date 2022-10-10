06ef26bd54bcb774ff781c5f3f8234f5fd96ae25
Harrison Royds <harrisonroyds@Harrisons-MacBook-Pro.local>
2022-10-10 09:57:15 +0100

    randomisation sorted

--- sorter.js
+++ sorter.js
@@ -1,27 +1,29 @@
 const { counsellors } = require("./counsellors.js");
 
 const sorter = (counsellors) => {
-  let shuffledCounsellors = counsellors.sort(() => Math.random() - 0.5);
-
-  let mondayArray = ["M"];
-  let tuesdayArray = ["Tu"];
-  let wednesdayArray = ["W"];
-  let thursdayArray = ["Th"];
+  let shuffledCounsellors = counsellors.sort(() => Math.random() - 0.5); //order of counsellors added to arrays is now random
+  // let counsellorNumber = counsellors.length;
+  let mondayArray = [];
+  let tuesdayArray = [];
+  let wednesdayArray = [];
+  let thursdayArray = [];
 
   let weekArray = [mondayArray, tuesdayArray, wednesdayArray, thursdayArray];
-  let shuffledWeekArray = weekArray.sort(() => Math.random() - 0.5);
+  let shuffledWeekArray = weekArray.sort(() => Math.random() - 0.5); //the days which recieve extra counsellors is now random
+  let adderCounter = 1;
 
-  console.log(shuffledWeekArray, shuffledCounsellors);
   for (let i = 0; i < shuffledCounsellors.length; i++) {
-    //Now push to arrays in order (with them filling based upon the number of counsellors divided by 4)
+    shuffledWeekArray[adderCounter].push(shuffledCounsellors[i]);
+    adderCounter++;
+    if (adderCounter == 5) {
+      adderCounter = 1;
+    }
+
     //Add conditional logic to prevent counsellors with the same schedule being matched
   }
 
   return {
-    Monday: mondayArray,
-    Tuesday: tuesdayArray,
-    Wednesday: wednesdayArray,
-    Tursday: thursdayArray,
+    shuffledWeekArray,
   };
 };
 
