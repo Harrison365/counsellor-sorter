fff267d1191fae48570e9547ba00b075cf79c2b1
Harrison365 <harrisonroyds@yahoo.co,uk>
2022-10-26 19:06:22 +0100

    minor comment changes

--- sorter.js
+++ sorter.js
@@ -2,14 +2,13 @@ const { counsellors } = require("./counsellors.js");
 
 const sorter = (counsellors) => {
   let shuffledCounsellors = counsellors.sort(() => Math.random() - 0.5); //order of counsellors added to arrays is now random
-  // let counsellorNumber = counsellors.length;
   let mondayArray = [];
   let tuesdayArray = [];
   let wednesdayArray = [];
   let thursdayArray = [];
 
   let weekArray = [mondayArray, tuesdayArray, wednesdayArray, thursdayArray];
-  let shuffledWeekArray = weekArray.sort(() => Math.random() - 0.5); //the days which recieve extra counsellors is now random
+  let shuffledWeekArray = weekArray.sort(() => Math.random() - 0.5); //the days which recieve extra counsellors is now random.
   let adderCounter = 0;
 
   for (let i = 0; i < shuffledCounsellors.length; i++) {
